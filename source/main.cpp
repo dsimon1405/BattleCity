0d42aec8e58fae8fe5bace2bfd74cb76e0096392
simon007 <simonchikd@internet.ru>
2023-02-28 00:20:38 +0300

    "First tiangle in OpenGL

--- source/main.cpp
+++ source/main.cpp
@@ -3,6 +3,36 @@
 
 #include <iostream>
 
+GLfloat point[] = {
+      0.0f,  0.5f, 0.0f,
+      0.5f, -0.5f, 0.0f,
+    - 0.5f, -0.5f, 0.0f
+};
+
+GLfloat colors[] = {
+    1.0f, 0.0f, 0.0f,
+    0.0f, 1.0f, 0.0f,
+    0.0f, 0.0f, 1.0f
+};
+
+const char* vertex_shader =
+"#version 460\n"    // версия OpenGL 4.6
+"layout(location = 0) in vec3 vertex_position;"
+"layout(location = 1) in vec3 vertex_color;"
+"out vec3 color;"
+"void main () {"
+"   color = vertex_color;"
+"   gl_Position = vec4(vertex_position, 1.0);"
+"}";
+
+const char* fragment_shader =
+"#version 460\n"
+"in vec3 color;"
+"out vec4 frag_color;"
+"void main () {"
+"   frag_color = vec4(color, 1.0);"
+"}";
+
 int windowSizeX = 640, windowSizeY = 480;
 
 void glfwWindowSizeCallback(GLFWwindow* pWindow, int width, int height)
@@ -61,11 +91,63 @@ int main(void)
 	
 	glClearColor(1,0,1,1);
 
+
+    // создание шейдеров
+    // vertex shader
+    GLuint vs = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vs, 1, &vertex_shader, nullptr);
+    glCompileShader(vs);
+
+    // fragment shader
+    GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fs, 1, &fragment_shader, nullptr);
+    glCompileShader(fs);
+
+    // создание программы для линковки созданных шейдеров
+    GLuint shader_program = glCreateProgram();
+    glAttachShader(shader_program, vs);
+    glAttachShader(shader_program, fs);
+    glLinkProgram(shader_program);
+
+    // после линковки шейдеры можно удалять
+    glDeleteShader(vs);
+    glDeleteShader(fs);
+
+    // передача в память видеокарты информации (позиция, цвет) для созданных шейдеров
+    GLuint points_vbo = 0;  // vertex buffer object
+    glGenBuffers(1, &points_vbo);   // генерация буфера (функция вернёт значение в points_vbo)
+    glBindBuffer(GL_ARRAY_BUFFER, points_vbo);  // подключение буфера (сделали буфер текущим)
+    glBufferData(GL_ARRAY_BUFFER, sizeof(point), point, GL_STATIC_DRAW);   // передача текущему буферу координат (на видеокарте размещение произайдёт в статической пмяти, данные не будут часто менятся)
+
+    GLuint colors_vbo = 0;
+    glGenBuffers(1, &colors_vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, colors_vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(colors), colors, GL_STATIC_DRAW); // перемещает данные из оператвной памяти процессора в память видеокарты
+
+    GLuint vao = 0;     // vertex array object
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+
+    glEnableVertexAttribArray(0);   // включение нулевой позиции в шейдере (содержит положение точки)
+    glBindBuffer(GL_ARRAY_BUFFER, points_vbo);  // сделать буфер текущим
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+
+    glEnableVertexAttribArray(1);
+    glBindBuffer(GL_ARRAY_BUFFER, colors_vbo);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(pWindow))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
+
+
+        glUseProgram(shader_program);   // подключение созданной программы линковки шейдеров
+        glBindVertexArray(vao);     // биндим эррей объект который хотим отрисовать, на данный момент он всего 1
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
 
         /* Swap front and back buffers */
         glfwSwapBuffers(pWindow);
